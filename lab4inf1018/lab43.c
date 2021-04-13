f6533819fff19d75103caa11762d20a09216919f
pedr0piquet <61810017+pedr0piquet@users.noreply.github.com>
2021-04-13 13:34:50 -0300

    Add files via upload

---
+++ lab4inf1018/lab43.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void dump (void *p, int n) {
+  	unsigned char *p1 = p;
+  	while (n--) {
+    		printf("%p - %02x\n", p1, *p1);
+		p1++;
+  }
+}
+
+
+int main()
+{
+  	signed char sc = -1;
+  	unsigned int ui = sc;
+
+  	printf("dump de l: \n");
+  	dump(&ui, sizeof(ui));
+
+	return 0;
+}
